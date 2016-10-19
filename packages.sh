361c4e0295fd23e495c370ad4e0c467eb763a5e4
Viren Mohindra <viren.mohindra@gmail.com>
2016-10-19 18:55:47 -0400

    Pushing .debs to repo
    Former-commit-id: 80da5a60b4a6cb900da99e9bcf916ad04937dafa [formerly 0bf626eaf04bb28d18c4665659055168c4bfb591]
    Former-commit-id: b3a90dff39c42c03bf40171fd9429b17cd23f10c

--- packages.sh
+++ packages.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/A Wave debs
-dpkg-deb -bZgzip projects/Bipolar Balls debs
+dpkg-deb -bZgzip projects/A\ Wave debs
+dpkg-deb -bZgzip projects/Bipolar\ Balls debs
 dpkg-deb -bZgzip projects/Bubbles debs
 dpkg-deb -bZgzip projects/Columns debs
 dpkg-deb -bZgzip projects/Complicated debs
 dpkg-deb -bZgzip projects/Echo debs
 dpkg-deb -bZgzip projects/Flower debs
 dpkg-deb -bZgzip projects/Fragments debs
-dpkg-deb -bZgzip projects/Funny Computer debs
+dpkg-deb -bZgzip projects/Funny\ Computer debs
 dpkg-deb -bZgzip projects/Hypnotoad debs
 dpkg-deb -bZgzip projects/Mograph debs
 dpkg-deb -bZgzip projects/Pizza debs
-dpkg-deb -bZgzip projects/Pizza Hi debs
+dpkg-deb -bZgzip projects/Pizza\ Hi debs
 dpkg-deb -bZgzip projects/Pokemon debs
 dpkg-deb -bZgzip projects/Pulse debs
-dpkg-deb -bZgzip projects/Rolling Circles debs
+dpkg-deb -bZgzip projects/Rolling\ Circles debs
 dpkg-deb -bZgzip projects/Routine debs
 dpkg-deb -bZgzip projects/Soup debs
 dpkg-deb -bZgzip projects/Spikey debs
 dpkg-deb -bZgzip projects/Sploosh debs
-dpkg-deb -bZgzip projects/Stay In Lane debs
+dpkg-deb -bZgzip projects/Stay\ In\ Lane debs
 dpkg-deb -bZgzip projects/Swimmer debs
 dpkg-deb -bZgzip projects/Swish debs
 dpkg-deb -bZgzip projects/Tesseract debs
-dpkg-deb -bZgzip projects/Three Waves debs
-dpkg-deb -bZgzip projects/Time And Money debs
-dpkg-deb -bZgzip projects/Twisting Cubes debs
+dpkg-deb -bZgzip projects/Three\ Waves debs
+dpkg-deb -bZgzip projects/Time\ And\ Money debs
+dpkg-deb -bZgzip projects/Twisting\ Cubes debs
 dpkg-deb -bZgzip projects/Upload debs
 dpkg-deb -bZgzip projects/Waiting debs
-dpkg-deb -bZgzip projects/World Swap debs
-dpkg-deb -bZgzip projects/Yin And Yang debs
+dpkg-deb -bZgzip projects/World\ Swap debs
+dpkg-deb -bZgzip projects/Yin\ And\ Yang debs
