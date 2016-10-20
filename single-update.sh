c10fb710538de2bd5a0585035c6dbe90bdab1575
Viren Mohindra <viren.mohindra@gmail.com>
2016-10-20 07:36:54 -0400

    Added Rainbow 8
    Former-commit-id: 4f618eb707deb034dbc0ff1d929859edb2246468 [formerly f095a30021e6597558e39e845f282fc1eb088a54]
    Former-commit-id: 053fe916c2ee6c78bff2d0caa9f5872e43d69f3e

--- single-update.sh
+++ single-update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# convert to CLI remove hard-coding
+# convert to CLI || remove hard-coding
+# Remove .deb if updating repo.
+rm -r Packages.bz2
 dpkg-deb -bZgzip projects/Hello\ Again debs
 dpkg-scanpackages ./debs > Packages
 bzip2 -fks Packages
