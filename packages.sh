0e8fa741a248ad490bcc9ef060ece87c2011f95f
Viren Mohindra <viren.mohindra@gmail.com>
2016-10-24 05:18:22 -0400

    bumped up to v2.0
    - added depictions
    - cleaned up animations
    - optimizing images
    
    
    Former-commit-id: 239675172f59b1f823b3009371dc41cab517e0d8 [formerly 7adb791506b4ac3667130fedf5f91c469fe12e03]
    Former-commit-id: f828d1373e978f5cad60d0381e4de3fec61bb519

--- packages.sh
+++ packages.sh
@@ -2,25 +2,34 @@
 dpkg-deb -bZgzip projects/A\ Wave debs
 dpkg-deb -bZgzip projects/Bipolar\ Balls debs
 dpkg-deb -bZgzip projects/Bubbles debs
+dpkg-deb -bZgzip projects/Colorful\ Stars\ In\ Space debs
 dpkg-deb -bZgzip projects/Columns debs
 dpkg-deb -bZgzip projects/Complicated debs
 dpkg-deb -bZgzip projects/Echo debs
 dpkg-deb -bZgzip projects/Flower debs
 dpkg-deb -bZgzip projects/Fragments debs
 dpkg-deb -bZgzip projects/Funny\ Computer debs
-dpkg-deb -bZgzip projects/Hypnotoad debs
+dpkg-deb -bZgzip projects/Gameboy\ Advance debs
+dpkg-deb -bZgzip projects/Gamecube\ Logo debs
 dpkg-deb -bZgzip projects/Hello\ Again debs
+dpkg-deb -bZgzip projects/Hypnotoad debs
 dpkg-deb -bZgzip projects/Mograph debs
+dpkg-deb -bZgzip projects/Pepe\ Matrix debs
 dpkg-deb -bZgzip projects/Pizza debs
 dpkg-deb -bZgzip projects/Pizza\ Hi debs
 dpkg-deb -bZgzip projects/Pokemon debs
 dpkg-deb -bZgzip projects/Pulse debs
+dpkg-deb -bZgzip projects/Rainbow\ 8 debs
+dpkg-deb -bZgzip projects/Rainy debs
 dpkg-deb -bZgzip projects/Rolling\ Circles debs
 dpkg-deb -bZgzip projects/Routine debs
+dpkg-deb -bZgzip projects/Sonic\ Running debs
 dpkg-deb -bZgzip projects/Soup debs
 dpkg-deb -bZgzip projects/Spikey debs
 dpkg-deb -bZgzip projects/Sploosh debs
 dpkg-deb -bZgzip projects/Stay\ In\ Lane debs
+dpkg-deb -bZgzip projects/Stranger\ Things debs
+dpkg-deb -bZgzip projects/Super\ Mario debs
 dpkg-deb -bZgzip projects/Swimmer debs
 dpkg-deb -bZgzip projects/Swish debs
 dpkg-deb -bZgzip projects/Tesseract debs
