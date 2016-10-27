bc518c05f41b1e816d790a2d3d459e3e4ff70da3
Viren Mohindra <viren.mohindra@gmail.com>
2016-10-27 01:07:55 -0400

    GA Integration + Added spinning rings deb
    Former-commit-id: 1fb2cff717f210e460051165eb06af2523933ff5 [formerly ef7c9b4f91578ec088bfceedccad6d118cd902c9]
    Former-commit-id: b5a0a162b750f99fef616de5849448db0702e0c9

--- packages.sh
+++ packages.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/A\ Wave debs
+dpkg-deb -bZgzip projects/Alone debs
 dpkg-deb -bZgzip projects/Bipolar\ Balls debs
+dpkg-deb -bZgzip projects/Black\ Hole debs
+dpkg-deb -bZgzip projects/Boo debs
 dpkg-deb -bZgzip projects/Bubbles debs
 dpkg-deb -bZgzip projects/Colorful\ Stars\ In\ Space debs
 dpkg-deb -bZgzip projects/Columns debs
 dpkg-deb -bZgzip projects/Complicated debs
 dpkg-deb -bZgzip projects/Echo debs
+dpkg-deb -bZgzip projects/Fall\ Leaves debs
 dpkg-deb -bZgzip projects/Flower debs
+dpkg-deb -bZgzip projects/Fluid debs
 dpkg-deb -bZgzip projects/Fragments debs
 dpkg-deb -bZgzip projects/Funny\ Computer debs
 dpkg-deb -bZgzip projects/Gameboy\ Advance debs
 dpkg-deb -bZgzip projects/Gamecube\ Logo debs
 dpkg-deb -bZgzip projects/Hello\ Again debs
 dpkg-deb -bZgzip projects/Hypnotoad debs
+dpkg-deb -bZgzip projects/Lines debs
 dpkg-deb -bZgzip projects/Mograph debs
+dpkg-deb -bZgzip projects/Only\ Human debs
 dpkg-deb -bZgzip projects/Pepe\ Matrix debs
 dpkg-deb -bZgzip projects/Pizza debs
 dpkg-deb -bZgzip projects/Pizza\ Hi debs
@@ -27,6 +34,7 @@ dpkg-deb -bZgzip projects/Sonic\ Running debs
 dpkg-deb -bZgzip projects/Soup debs
 dpkg-deb -bZgzip projects/Spikey debs
 dpkg-deb -bZgzip projects/Sploosh debs
+dpkg-deb -bZgzip projects/Spinning\ Rings debs
 dpkg-deb -bZgzip projects/Stay\ In\ Lane debs
 dpkg-deb -bZgzip projects/Stranger\ Things debs
 dpkg-deb -bZgzip projects/Super\ Mario debs
@@ -40,20 +48,3 @@ dpkg-deb -bZgzip projects/Upload debs
 dpkg-deb -bZgzip projects/Waiting debs
 dpkg-deb -bZgzip projects/World\ Swap debs
 dpkg-deb -bZgzip projects/Yin\ And\ Yang debs
-
-
-# reorganize
-dpkg-deb -bZgzip projects/Alone debs
-dpkg-deb -bZgzip projects/Black\ Hole debs
-dpkg-deb -bZgzip projects/Only\ Human debs
-dpkg-deb -bZgzip projects/Lines debs
-dpkg-deb -bZgzip projects/Fluid debs
-dpkg-deb -bZgzip projects/Boo debs
-dpkg-deb -bZgzip projects/Fall\ Leaves debs
-
-
-
-
-
-
-
