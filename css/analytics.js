8123d8dfb586a44e8768673fe0db410c82fd75d5
Viren Mohindra <viren.mohindra@gmail.com>
2016-10-27 07:51:33 -0400

    added linkid
    Former-commit-id: e68d306bf03959a108247c4733ca60f476ca2d4d [formerly 160330ed17964f0d19ad6173f407b0d63c26680f]
    Former-commit-id: 57120cb711cbd77d5ffebb1eff9017d56b761b78

--- css/analytics.js
+++ css/analytics.js
@@ -3,4 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-31897403-4', 'auto');
+ga('require', 'linkid');
 ga('send', 'pageview');
